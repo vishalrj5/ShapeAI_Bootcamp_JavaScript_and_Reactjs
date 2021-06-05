b55c488357ec84b267d9e3c6c29bb5a097f782f8
vishalrj5 <vishalrj5@gmail.com>
2021-06-05 20:42:40 +0530

    ShapeAI project

---
+++ src/components/Notes.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Note(){
+  return(
+    <div>
+      <div className="content"><h3>Full-Stack Web Dev Training and Internship</h3><p>The goal of this Training and Internship Program is to equip interns with the unique skills they need to build database-backed APIs and web applications with the proper industry level front-end as well as scale up the deployment for the millions. A graduate of this program will be able to design and build databases for software applications, create and deploy database-backed web APIs, and secure and manage user authentication and access control for an application backend. They will be able to build production level frontend according to the industrial standards. Students will also learn how to deploy a the entire web application to the cloud using Docker and Kubernetes and how to maintain and scale them.</p></div>
+      <div className="content"><h3>Data Analyst Training And Internship</h3><p>The Goal of this Training and Internship program is to Advance your programming skills and refine your ability to work with messy, complex datasets. You’ll learn to manipulate and prepare data for analysis, and create visualizations for data exploration. Finally, you’ll learn to use your data skills to tell a story with data. In this program, you’ll learn foundational data skills that apply across functions and industries. You’ll learn to analyze data and build models with Excel, query databases using SQL, and create informative data visualizations with Python and its Libraries.</p></div>
+      <div className="content"><h3>Web Dev with Javascript and React.js BootCamp</h3><p>The Goal of this Bootcamp is to introduce HTML,CSS,Javascrip,React.js to students.Reactjs can be easily labeled as a bestseller. Launched back in 2013, this JavaScript library has quickly won popular affections. Today it is maintained by Facebook, together with the developer community. It is used by the leading companies, like Apple, PayPal, Netflix, while over 32 thousand of websites are built using React JS framework.</p></div>
+    </div>
+  );
+}
+
+export default Note;
